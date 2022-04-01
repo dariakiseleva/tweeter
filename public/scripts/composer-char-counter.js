9f1b49778edf8af84b190fd05ff7e739db32774a
daria <daria.kiseleva@mail.mcgill.ca>
2022-03-31 21:07:24 -0400

    Add event listener to textarea where the user writes the tweet, log remaining characters to console

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,22 @@
 //Responsible for character counter
 
+////----------CALLBACK FUNCTIONS
+
+const testCallback = function(event){
+  const chars = $(this).val().length;
+  const remChars = 140 - chars;
+  console.log(remChars);
+}
+
+
+
+////----------EXECUTE
+
 $(document).ready(function() {
-  // --- our code goes here ---
+
+  $( "#tweet-text" )
+  .on('input', testCallback);
+
 });
 
 
-console.log("I'm ready!");
