7560e9c461cfa4faeabf26de04538a6398455e9b
Daria Kiseleva <daria.kiseleva@mail.mcgill.ca>
2022-04-06 22:18:56 +0000

    Add tweet input validation for length. Rearrange function calls so that new tweets are loaded upon tweeting at the top of the container.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -35,7 +35,7 @@ const createTweetElement = (data) => {
 //Takes array of objects with info about tweets, calls createTweetElemenet and appends resulting HTML to the page
 const renderTweets = function(tweets) {
   $('#tweet-container').empty();
-  for (let tweet of tweets){
+  for (let tweet of tweets.reverse()){
     $('#tweet-container').append(createTweetElement(tweet));
   }
 }
@@ -43,9 +43,24 @@ const renderTweets = function(tweets) {
 //Prevent the form from changing page on submission and send an AJAX post request with the data
 const handleFormSubmission = () => {
   $('main > .new-tweet > form').submit((event) => {
+
+    //Prevent form submission that refreshes page
     event.preventDefault();
+
+    //Validate that tweet input is not empty or too long
+    const userInput = $("#tweet-text").val();
+    if (userInput.length==0) {
+      return alert("Tweet can not be empty.");
+    } else if (userInput.length > 140){
+      return alert("Tweet can not exceed 140 characters");
+    }
+
+    //Post the tweet to server
     const submission = $('main > .new-tweet > form').serialize();
     $.ajax({url: '/tweets', method: 'POST', dataType: "xhr", data: submission});
+
+    //Refresh tweets
+    loadTweets();
 
   });
 }
@@ -60,6 +75,6 @@ const loadTweets = () => {
 
 //-----Execute after loading DOM
 $(()=> {
-  handleFormSubmission();
-  loadTweets();
+  loadTweets(); //Initial tweet loading
+  handleFormSubmission(); 
 });
